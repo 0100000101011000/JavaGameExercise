b765991f4681f8302189ffad5fa59ccb3984a004
0100000101011000 <alex92_guckheim@web.de>
2023-02-09 20:56:20 +0100

    update

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -7,6 +7,7 @@ import static utilz.Constants.Directions.UP;
 import static utilz.Constants.PlayerConstants.GetSpriteAmount;
 import static utilz.Constants.PlayerConstants.IDLE;
 import static utilz.Constants.PlayerConstants.RUNNING;
+import static utilz.Constants.PlayerConstants.ATTACK_1;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
@@ -15,14 +16,17 @@ import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
+import utilz.LoadSave;
+
 public class Player extends Entity{
 
 	private BufferedImage[][] animations;
-	private int aniTick, aniIndex, aniSpeed = 15;
+	private int aniTick, aniIndex, aniSpeed = 20;
 	private int playerAction = IDLE;
 	private boolean left, up, right, down;
-	private boolean moving = false;
+	private boolean moving = false, attacking = false;
 	private float playerSpeed = 2.0f;
+	private float diagonal = 0.0f;
 	private int playerDir = -1;
 	
 	public Player(float x, float y) {
@@ -53,70 +57,85 @@ public class Player extends Entity{
 	private void updateAnimationTick() {
 		
 		aniTick++;
-		if(aniTick >= aniSpeed) {
+		if (aniTick >= aniSpeed) {
 			aniTick = 0;
 			aniIndex++;
-			if(aniIndex >= GetSpriteAmount(playerAction)) {
+			if (aniIndex >= GetSpriteAmount(playerAction)) {
 				aniIndex = 0;
+				attacking = false;
 			}
 		}
 	}
 	
 	private void setAnimation() {
 		
-		if(moving)
+		int startAni = playerAction;
+		
+		if (moving)
 			playerAction = RUNNING;
 		else
 			playerAction = IDLE;
 		
+		if(attacking)
+			playerAction = ATTACK_1;
+		
+		if (startAni != playerAction)
+			resetAnimationIndex();
+		
 	}
 	
+	private void resetAnimationIndex() {
+		aniTick = 0;
+		aniIndex = 0;
+		
+	}
+
 	private void updatePos() {
 		
 		moving = false;
+		diagonal = 0.0f;
 		
-		if(left && !right) {
-			x -= playerSpeed;
+		if ((up || down) && (left || right))
+			diagonal = playerSpeed * 0.314f;
+		
+		
+		if (left && !right) {
+			x -= (playerSpeed - diagonal);
 			moving = true;
-		} else if(right && !left) {
-			x += playerSpeed;
+		} else if (right && !left) {
+			x += (playerSpeed -diagonal);
 			moving = true;
 		}
 		
-		if(up && !down) {
-			y -= playerSpeed;
+		if (up && !down) {
+			y -= (playerSpeed - diagonal);
 			moving = true;
-		} else if(down && !up) {
-			y += playerSpeed;
+		} else if (down && !up) {
+			y += (playerSpeed - diagonal);
 			moving = true;
 		}
+		
+		
+			
 	}
 	
 	private void loadAnimations() {
 		
-		InputStream is = getClass().getResourceAsStream("/player_sprites.png");
-		try {
-			BufferedImage img = ImageIO.read(is);
-			
-			animations = new BufferedImage[9][6];			
-			for(int j=0; j<animations.length; j++) {
-				for(int i=0; i<animations[j].length; i++) {
-					animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				is.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}	
+		BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
+		
+		animations = new BufferedImage[9][6];	
+		for (int j=0; j<animations.length; j++)
+			for (int i=0; i<animations[j].length; i++)
+				animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
+	
+	}
+	
+	public void setAttacking(boolean attacking) {
+		this.attacking = attacking;
 	}
 
 	public boolean isLeft() {
-		return left;
+		return left;	
 	}
 
 	public void setLeft(boolean left) {
--- src/main/Game.java
+++ src/main/Game.java
@@ -3,21 +3,31 @@ package main;
 import java.awt.Graphics;
 
 import entities.Player;
+import levels.LevelManager;
 
 public class Game implements Runnable{
 	
-	private GameWindow gameWindow;
 	private GamePanel gamePanel;
 	private Thread gameThread;
 	private final int FPS_SET = 150;
-	private final int UPS_SET = 200;
-	
+	private final int UPS_SET = 200;	
 	private Player player;
+	private LevelManager levelManager;
+	
+	public static final int TILES_DEFAULT_SIZE = 32;
+	public static final float SCALE = 1.5f;
+	public static final int TILES_IN_WIDTH = 26;
+	public static final int TILES_IN_HEIGHT = 14;
+	public static final int TILES_SIZE = (int) (TILES_DEFAULT_SIZE * SCALE);
+	public static final int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
+	public static final int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
+	
+
 
 	public Game() {
 		initClasses();
 		gamePanel = new GamePanel(this);
-		gameWindow = new GameWindow(gamePanel);
+		new GameWindow(gamePanel);
 		gamePanel.requestFocus();		
 		startGameLoop();
 		
@@ -26,7 +36,7 @@ public class Game implements Runnable{
 	
 	private void initClasses() {
 		player = new Player(200, 200);
-		
+		levelManager = new LevelManager(this);
 	}
 
 	private void startGameLoop() {
@@ -36,10 +46,12 @@ public class Game implements Runnable{
 	
 	public void update() {
 		player.update();
+		levelManager.update();
 	}
 	
 	public void render(Graphics g) {
 		player.render(g);
+		levelManager.draw(g);
 	}
 
 	@Override
@@ -56,7 +68,7 @@ public class Game implements Runnable{
 		int frames = 0;
 		int updates = 0;
 		long lastCheck = System.currentTimeMillis();
-		while(true) {			
+		while(true) {
 			long currentTime = System.nanoTime();
 			
 			// deltaU neutralize lags for the FPS
@@ -64,6 +76,7 @@ public class Game implements Runnable{
 			deltaU += (currentTime - previousTime) / timePerUpdate;
 			deltaF += (currentTime - previousTime) / timePerFrame;
 			previousTime = currentTime;
+			
 			
 			if(deltaU >= 1) {
 				update();
